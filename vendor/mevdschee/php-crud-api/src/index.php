7859f5e30e45e61ccf6a41a25b9ca18a6b0f04d7
unknown <rajkumar.ganni@gmail.com>
2021-02-16 01:00:59 +0800

    Added to cms-crud-api

---
+++ vendor/mevdschee/php-crud-api/src/index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tqdev\PhpCrudApi;
+
+use Tqdev\PhpCrudApi\Api;
+use Tqdev\PhpCrudApi\Config;
+use Tqdev\PhpCrudApi\RequestFactory;
+use Tqdev\PhpCrudApi\ResponseUtils;
+
+require '../vendor/autoload.php';
+
+$config = new Config([
+    // 'driver' => 'mysql',
+    // 'address' => 'localhost',
+    // 'port' => '3306',
+    'username' => 'php-crud-api',
+    'password' => 'php-crud-api',
+    'database' => 'php-crud-api',
+    // 'debug' => false
+]);
+$request = RequestFactory::fromGlobals();
+$api = new Api($config);
+$response = $api->handle($request);
+ResponseUtils::output($response);
